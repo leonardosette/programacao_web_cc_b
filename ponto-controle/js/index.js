e7061b9cf384f526aa4e93bd909e648b994558df
Leonardo Torres de Oliveira <leonardo.to@dinfor.uniceub>
2024-09-03 09:11:45 -0300

    atualizacoes e implementacoes

--- ponto-controle/js/index.js
+++ ponto-controle/js/index.js
@@ -10,3 +10,32 @@ function atualizarDataHora() {
 setInterval(atualizarDataHora, 1000);
 // Atualiza a hora imediatamente quando o fuso horário é alterado
 document.getElementById('fusoHorario').addEventListener('change', atualizarDataHora);
+
+
+document.getElementById('baterPonto').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'block';
+  });
+  
+  document.getElementById('close').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+  });
+  
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+      document.getElementById('modal').style.display = 'none';
+    }
+  });
+  
+  document.getElementById('entrada').addEventListener('click', function() {
+    alert('Entrada registrada');
+  });
+  
+  document.getElementById('saida').addEventListener('click', function() {
+    alert('Saida registrada');
+  });
+  document.getElementById('intervalo').addEventListener('click', function() {
+    alert('Intervalo registrado');
+  });
+  document.getElementById('voltaIntervalo').addEventListener('click', function() {
+    alert('Volta do intervalo registrada');
+  });
