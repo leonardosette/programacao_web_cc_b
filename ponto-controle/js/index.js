374b80caf480e9742eb256e25f3f323ce2233db9
Leonardo Torres de Oliveira <leonardo.to@dinfor.uniceub>
2024-09-03 10:29:29 -0300

    improv

--- ponto-controle/js/index.js
+++ ponto-controle/js/index.js
@@ -1,41 +1,50 @@
 function atualizarDataHora() {
     var agora = new Date();
     var fusoHorario = document.getElementById('fusoHorario').value;
-    var opcoes = { timeZone: fusoHorario};
-    var hora = agora.toLocaleTimeString('en-US',opcoes);
+    var opcoes = { timeZone: fusoHorario };
+    var hora = agora.toLocaleTimeString('en-US', opcoes);
     var data = agora.toLocaleDateString('en-US', opcoes);
     document.getElementById('dataHora').textContent = data + ' ' + hora;
-
 }
+
+function atualizarModalDataHora() {
+    var dataHora = document.getElementById('dataHora').textContent;
+    document.getElementById('modalDataHora').textContent = dataHora;
+}
+
 setInterval(atualizarDataHora, 1000);
+setInterval(atualizarModalDataHora, 1000);
+
 // Atualiza a hora imediatamente quando o fuso horário é alterado
 document.getElementById('fusoHorario').addEventListener('change', atualizarDataHora);
 
-
 document.getElementById('baterPonto').addEventListener('click', function() {
+    atualizarModalDataHora(); // Atualiza o modal com a data e hora atuais
     document.getElementById('modal').style.display = 'block';
-  });
-  
-  document.getElementById('close').addEventListener('click', function() {
+});
+
+document.getElementById('close').addEventListener('click', function() {
     document.getElementById('modal').style.display = 'none';
-  });
-  
-  document.addEventListener('keydown', function(event) {
+});
+
+document.addEventListener('keydown', function(event) {
     if (event.key === 'Escape') {
-      document.getElementById('modal').style.display = 'none';
+        document.getElementById('modal').style.display = 'none';
     }
-  });
-  
-  document.getElementById('entrada').addEventListener('click', function() {
+});
+
+document.getElementById('entrada').addEventListener('click', function() {
     alert('Entrada registrada');
-  });
-  
-  document.getElementById('saida').addEventListener('click', function() {
-    alert('Saida registrada');
-  });
-  document.getElementById('intervalo').addEventListener('click', function() {
+});
+
+document.getElementById('saida').addEventListener('click', function() {
+    alert('Saída registrada');
+});
+
+document.getElementById('intervalo').addEventListener('click', function() {
     alert('Intervalo registrado');
-  });
-  document.getElementById('voltaIntervalo').addEventListener('click', function() {
+});
+
+document.getElementById('voltaIntervalo').addEventListener('click', function() {
     alert('Volta do intervalo registrada');
-  });
+});
